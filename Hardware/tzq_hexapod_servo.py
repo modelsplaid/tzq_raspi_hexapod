88cad479622ddb6b7a03d9036f37c1d2ac3f33c4
ztang4 <1457311565@qq.com>
2021-11-23 22:42:42 +0800

    tested hiwonder api in Hardware folder

---
+++ Hardware/tzq_hexapod_servo.py
@@ -0,0 +1,11 @@
+# An interface between hexapod simulator and hiwonder hexapod
+import time
+
+import sys
+sys.path.append("../")
+
+from HiwonderSDK import Board
+Board.setBusServoPulse(1, 800, 1000)
+time.sleep(1)
+for i in range(19):
+    Board.unloadBusServo(i)
