3ad79c38af40adb1c9b0f5081757a57fc2c43934
zhiqiang tang <tangzhiqiang@yihang.ai>
2021-12-28 20:15:46 +0800

    tested consistance with js version ik. they are consistant. todo: add inverse call to walkSequenceSolve.py

--- tests/walk_sequence_test/walkSequenceSolver.py
+++ tests/walk_sequence_test/walkSequenceSolver.py
@@ -1,2 +1,27 @@
 def getWalkSequence(dimensions,params,gaitType = "tripod",walkMode = "walking"):
     print("in getWalkSequence") 
+
+    ik_parameters = {
+        "hip_stance": 25,
+        "leg_stance": 0,
+        "percent_x": 0.0,
+        "percent_y": 0.0,
+        "percent_z": 0.0,
+        "rot_x": 0,
+        "rot_y": 0,
+        "rot_z": 0,
+    }
+
+    '''
+     const { hipStance, rx, ry, tx, tz, legStance } = params
+    const rawIKparams = {
+        tx,
+        ty: 0,
+        tz,
+        legStance,
+        hipStance,
+        rx,
+        ry,
+        rz: 0,
+    }
+    '''
