b0e9837622e5036d992ff8df3af512c7c93b1da3
modelsplaid <modelsplaid@qq.com>
2022-06-03 18:10:36 +0800

    added json config for const.py

--- hexapod/const.py
+++ hexapod/const.py
@@ -4,6 +4,7 @@ from hexapod.models import VirtualHexapod, Hexagon, Linkage
 from hexapod.templates.figure_template import HEXAPOD_FIGURE
 from hexapod.templates.pose_template import HEXAPOD_POSE
 import sys
+import json
 sys.path.append("../../")
 try: 
     from Hardware import jointangle_to_pulse
@@ -18,25 +19,31 @@ except:
 NAMES_LEG = Hexagon.VERTEX_NAMES
 NAMES_JOINT = Linkage.POINT_NAMES
 
-BASE_DIMENSIONS = { # todo here 
-    "front": 59,
-    "side": 119,
-    "middle": 93,
-    "coxia": 45,
-    "femur": 75,
-    "tibia": 140,
-}
+# BASE_DIMENSIONS = { # todo here 
+#     "front": 59,
+#     "side": 119,
+#     "middle": 93,
+#     "coxia": 45,
+#     "femur": 75,
+#     "tibia": 140,
+# }
 
-BASE_IK_PARAMS = {
-    "hip_stance": 0,
-    "leg_stance": 0,
-    "percent_x": 0,
-    "percent_y": 0,
-    "percent_z": 0,
-    "rot_x": 0,
-    "rot_y": 0,
-    "rot_z": 0,
-}
+# BASE_IK_PARAMS = {
+#     "hip_stance": 0,
+#     "leg_stance": 0,
+#     "percent_x": 0,
+#     "percent_y": 0,
+#     "percent_z": 0,
+#     "rot_x": 0,
+#     "rot_y": 0,
+#     "rot_z": 0,
+# }
+out_file = open("./config/robot_dim_config.json", "r")
+config_json = json.load(out_file)
+BASE_DIMENSIONS = (config_json['BASE_DIMENSIONS'])
+BASE_IK_PARAMS = (config_json['BASE_IK_PARAMS'])
+print(BASE_DIMENSIONS['front'] )
+
 
 print("instance BASE_POSE")
 BASE_POSE = deepcopy(HEXAPOD_POSE)
